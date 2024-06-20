a2b6d104ca9a89d1fc62ebf8ac0b0ce653c3bd5b
Utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-06-21 01:09:03 +0530

    added star ratings

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import StarRating from "./components/StarRating";
 
 function App() {
   return (
     <>
-      <h1>Front-end Machine Coding Questions</h1>
+      <div className="App">
+        <h1>Front-end Machine Coding Questions</h1>
+        <StarRating />
+      </div>
     </>
   );
 }
---
+++ src/components/StarRating/index.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const StarRating = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  return (
+    <div>
+      <h1>Start Rating</h1>
+      {[1, 2, 3, 4, 5].map((num) => (
+        <button
+          key={num}
+          onClick={() => setRating(num)}
+          onMouseLeave={() => setHover(rating)}
+          onMouseOver={() => setHover(num)}
+        >
+          <span
+            className={`star ${
+              num <= ((rating && hover) || hover) ? "on" : "off"
+            }`}
+          >
+            &#9733;
+          </span>
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default StarRating;
