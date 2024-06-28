4c3729468067ed476f3f32c616319dfbd0e93916
Utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-06-28 23:07:23 +0530

    added progressbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import ProgressBar from "./components/ProgressBar";
 import StarRating from "./components/StarRating";
 
 function App() {
+  const [progress, setProgress] = useState(0);
+  useEffect(() => {
+    const time = setInterval(() => {
+      if (progress < 100) {
+        setProgress((p) => p + 1);
+      }
+    }, 20);
+
+    return () => {
+      clearInterval(time);
+    };
+  }, [progress]);
   return (
     <>
       <div className="App">
         <h1>Front-end Machine Coding Questions</h1>
-        <StarRating />
+        {/* <StarRating /> */}
+
+        {/* Progress Bar */}
+        {/* <div className="App">
+          <ProgressBar progress={progress} color="red" />{" "}
+        </div>
+        <div className="App">
+          <ProgressBar progress={progress} color="lightgreen" />{" "}
+        </div>
+        <div className="App">
+          <ProgressBar progress={progress} color="lightblue" />{" "}
+        </div>
+        <div className="App">
+          <ProgressBar progress={progress} color="yellow" />{" "}
+        </div> */}
       </div>
     </>
   );
---
+++ src/components/ProgressBar/index.jsx
@@ -0,0 +1,19 @@
+import react from "react";
+
+const ProgressBar = ({ progress, color }) => {
+  const styleObject = {
+    width: `${progress}%`,
+    backgroundColor: color || "red",
+    height: 30,
+    borderRadius: 20,
+  };
+  return (
+    <div className="container">
+      <div className="progress-bar">
+        <div style={styleObject}>{`${progress}%`}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
